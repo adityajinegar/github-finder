9e56d247be505fe18a775b42791b08dfd9e36280
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-02-07 12:38:30 -0500

    get single user

--- src/context/github/GitHubContext.jsx
+++ src/context/github/GitHubContext.jsx
@@ -9,6 +9,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GitHubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   };
 
@@ -33,6 +34,25 @@ export const GitHubProvider = ({ children }) => {
     dispatch({ type: 'GET_USERS', payload: items });
   };
 
+  // Get single user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (response.status === 404) {
+      window.location = '/notfound';
+    } else {
+      const data = await response.json();
+
+      dispatch({ type: 'GET_USER', payload: data });
+    }
+  };
+
   // Clear users from state
   const clearUsers = () => {
     dispatch({ type: 'CLEAR_USERS' });
@@ -45,9 +65,11 @@ export const GitHubProvider = ({ children }) => {
     <GitHubContext.Provider
       value={{
         users: state.users,
+        user: state.user,
         loading: state.loading,
         searchUsers,
         clearUsers,
+        getUser,
       }}
     >
       {children}
